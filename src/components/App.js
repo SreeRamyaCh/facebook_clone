9612537a00fd21c6bac7f7db9ef918c175c5fece
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-10-28 15:01:13 +0530

    changed the favicon and logos

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello React</h1>
       </div>
-    )
+    );
   }
 }
 
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
 
-test('renders learn react link', () => {
+test("renders Hello React", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
