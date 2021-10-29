befe13fdffa28a87158c4aefe9349f16361f3ca1
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-10-29 23:32:22 +0530

    Created Feedjs and styles

---
+++ src/components/Feed.js
@@ -0,0 +1,13 @@
+import FBBox from "./Bootstrap/FBBox";
+
+const Feed = () => {
+  return (
+    <div className="col-3">
+      <FBBox>Birthdays</FBBox>
+      <FBBox>Pages</FBBox>
+      <FBBox>Feed</FBBox>
+    </div>
+  );
+};
+
+export default Feed;
