dea5170a7f0a15cb05f4da928df13467fba3ecc7
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-10-30 00:43:28 +0530

    Birthday section Added

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,7 +3,13 @@ import FBBox from "./Bootstrap/FBBox";
 const Feed = () => {
   return (
     <div className="col-3">
-      <FBBox>Birthdays</FBBox>
+      <FBBox>
+        <div className="BirthdayContainer">
+          <img src="https://i.imgur.com/BSMfLfb.png" alt="Gift" />
+          <a href="/">Ramya</a>,<a href="/">Datta</a>,<a href="/">Sravya</a> and
+          3 others.
+        </div>
+      </FBBox>
       <FBBox>Pages</FBBox>
       <FBBox>Feed</FBBox>
     </div>
