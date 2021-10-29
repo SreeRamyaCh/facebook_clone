117c9826c69ba27ad24cd8e5b3a59969345a04d8
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-10-29 23:14:54 +0530

    Sidebar changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const sidebar = () => {
+  const ContentList = {
+    Default: [
+      {
+        Link: "/",
+        Icon: "news",
+        Text: "News Feed"
+      },
+      {
+        Link: "/",
+        Icon: "chat",
+        Text: "Messager"
+      },
+      {
+        Link: "/",
+        Icon: "watch",
+        Text: "Watch"
+      },
+      {
+        Link: "/",
+        Icon: "market",
+        Text: "Marketplae"
+      }
+    ],
+    Shortcuts: [
+      {
+        Link: "/",
+        Icon: "web",
+        Text: "Personal Website"
+      }
+    ],
+    Explorer: [
+      {
+        Link: "/",
+        Icon: "yt",
+        Text: "Youtube Channel"
+      }
+    ]
+  };
   return (
     <div className="Sidebar col-2">
       <div className="UserMeta">
@@ -9,6 +47,21 @@ const sidebar = () => {
           Ramya
         </a>
       </div>
+      {Object.keys(ContentList).map(section => (
+        <Fragment key={section}>
+          {section !== "Default" && <h3>{section}</h3>}
+          <ul className="Sidebar-Links">
+            {ContentList[section].map(link => (
+              <li key={link.Icon}>
+                <a href={link.Link}>
+                  <span className={"Sprite-" + link.Icon} />
+                  {link.Text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      ))}
     </div>
   );
 };
